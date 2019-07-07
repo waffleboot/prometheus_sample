608dd45da704a253114f321c7a00a12d472af3c2
Andrei Yangabishev <waffleboot@gmail.com>
2019-07-07 19:55:54 +0300

    middleware

--- internal/app/service/service.go
+++ internal/app/service/service.go
@@ -5,14 +5,16 @@ import (
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func Handler() http.HandlerFunc {
-	opsProcessed := promauto.NewCounter(prometheus.CounterOpts{
+	opsProcessed := promauto.NewCounterVec(prometheus.CounterOpts{
 		Name: "service_processed_ops_total",
 		Help: "the total number of processed events",
-	})
-	return func(w http.ResponseWriter, r *http.Request) {
-		opsProcessed.Inc()
-	}
+	}, []string{"method"})
+	return promhttp.InstrumentHandlerCounter(opsProcessed, http.HandlerFunc(handler))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
 }
